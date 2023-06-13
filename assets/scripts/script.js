8157a8f4de45e6e3e7cafaafe981b71c7d445f43
tinatin92 <tinatygordadze@gmail.com>
2023-06-13 18:31:23 +0400

    update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,7 +8,33 @@ $(document).ready(function () {
     speed: 1500,
   })
 
-
-
 });
 
+
+
+
+const  span= document.querySelector('.round');
+const dropdown = document.querySelector('#sub-nav');
+let isHovered = false;
+
+dropdown.addEventListener('mousemove', function(event) {
+  const dropdownRect = dropdown.getBoundingClientRect();
+  const maxTop = dropdownRect.height - span.offsetHeight;
+ 
+
+  const hoveredElement = document.elementFromPoint(event.clientX, event.clientY);
+
+  if (hoveredElement !== dropdown && hoveredElement !== span) {
+    const liRect = hoveredElement.getBoundingClientRect();
+    span.style.top = Math.max(0, Math.min(maxTop, liRect.top - dropdownRect.top)) + 'px';
+    isHovered = true;
+  } else {
+    isHovered = false;
+  }
+
+});
+dropdown.addEventListener('mouseleave', function() {
+  if (!isHovered) {
+    span.style.top = 0;
+  }
+});
